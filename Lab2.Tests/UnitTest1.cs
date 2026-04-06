6806e68b44cea5699af8225043c34386d199bacf
Oleksandr Unhurian <unguransasha@gmail.com>
2026-04-06 17:25:53 +0300

    Lab2 - Done

---
+++ Lab2.Tests/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+
+public class ProgramTests
+{
+    [Fact]
+    public void ReplaceLessThanK_Works()
+    {
+        double[] arr = { 1, 5, 2 };
+        double[] result = Program.ReplaceLessThanK(arr, 3);
+
+        Assert.Equal(new double[] { 3, 5, 3 }, result);
+    }
+
+    [Fact]
+    public void MinElementIndexes_Works()
+    {
+        double[] arr = { 5, 1, 3, 1 };
+
+        var result = Program.MinElementIndexes(arr);
+
+        Assert.Equal(new int[] { 1, 3 }, result);
+    }
+
+    [Fact]
+    public void SwapRowsMatrix_Even()
+    {
+        int[,] matrix =
+        {
+            {1,2},
+            {3,4}
+        };
+
+        var result = Program.SwapRowsMatrix(matrix);
+
+        Assert.Equal(3, result[0,0]);
+        Assert.Equal(4, result[0,1]);
+    }
+
+    [Fact]
+    public void SwapRowsMatrix_Odd()
+    {
+        int[,] matrix =
+        {
+            {1,2},
+            {3,4},
+            {5,6}
+        };
+
+        var result = Program.SwapRowsMatrix(matrix);
+
+        Assert.Equal(3, result[0,0]);
+    }
+
+    [Fact]
+    public void FirstPositivePerColumn_Works()
+    {
+        int[][] arr =
+        {
+            new int[] { -1, 2, -3 },
+            new int[] { 4, -5, 6 }
+        };
+
+        var result = Program.FirstPositivePerColumn(arr);
+
+        Assert.Equal(new int[] { 4, 2, 6 }, result);
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,256 @@
+﻿using System; 
+
+public class Program
+{
+
+    public static double[] ReplaceLessThanK(double[] arr, double k)
+{
+    double[] result = new double[arr.Length];
+
+    for (int i = 0; i < arr.Length; i++)
+        result[i] = arr[i] < k ? k : arr[i];
+
+    return result;
+}
+
+public static int[] MinElementIndexes(double[] arr)
+{
+    double min = arr[0];
+
+    for (int i = 1; i < arr.Length; i++)
+        if (arr[i] < min)
+            min = arr[i];
+
+    List<int> indexes = new List<int>();
+
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] == min)
+            indexes.Add(i);
+
+    return indexes.ToArray();
+}
+
+public static int[,] SwapRowsMatrix(int[,] a)
+{
+    int n = a.GetLength(0); // рядки
+    int m = a.GetLength(1); // стовпці
+
+    int[,] result = (int[,])a.Clone();
+
+    if (n % 2 == 0)
+    {
+        int r1 = n / 2 - 1;
+        int r2 = n / 2;
+
+        for (int j = 0; j < m; j++)
+        {
+            int temp = result[r1, j];
+            result[r1, j] = result[r2, j];
+            result[r2, j] = temp;
+        }
+    }
+    else
+    {
+        int mid = n / 2;
+
+        for (int j = 0; j < m; j++)
+        {
+            int temp = result[0, j];
+            result[0, j] = result[mid, j];
+            result[mid, j] = temp;
+        }
+    }
+
+    return result;
+}
+
+public static int[] FirstPositivePerColumn(int[][] arr)
+{
+    int maxCols = arr.Max(r => r.Length);
+    int[] result = new int[maxCols];
+
+    for (int j = 0; j < maxCols; j++)
+    {
+        result[j] = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (j < arr[i].Length && arr[i][j] > 0)
+            {
+                result[j] = arr[i][j];
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+    static int ReadInt(string msg)
+    {
+        while (true)
+        {
+            Console.Write(msg);
+            if (int.TryParse(Console.ReadLine(), out int x))
+                return x;
+            Console.WriteLine("Помилка!");
+        }
+    }
+
+    static double ReadDouble(string msg)
+    {
+        while (true)
+        {
+            Console.Write(msg);
+            if (double.TryParse(Console.ReadLine(), out double x))
+                return x;
+            Console.WriteLine("Помилка!");
+        }
+    }
+
+    static void Task1()
+    {
+        int n = ReadInt("Розмір масиву: ");
+        double[] arr = new double[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = ReadDouble($"arr[{i}] = ");
+
+        double k = ReadDouble("Введіть число: ");
+
+        for (int i = 0; i < n; i++)
+            if (arr[i] < k)
+                arr[i] = k;
+
+        Console.WriteLine("Результат:");
+        foreach (var x in arr)
+            Console.Write(x + " ");
+        Console.WriteLine();
+    }
+
+    static void Task2()
+    {
+        int n = ReadInt("Кількість елементів: ");
+        double[] arr = new double[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = ReadDouble($"a[{i}] = ");
+
+        double min = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] < min)
+                min = arr[i];
+
+        Console.WriteLine("Мінімальні елементи на позиціях:");
+        for (int i = 0; i < n; i++)
+            if (arr[i] == min)
+                Console.Write(i + " ");
+        Console.WriteLine();
+    }
+
+    static void Task3()
+    {
+        int n = ReadInt("Розмір матриці n: ");
+        int[,] a = new int[n, n];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                a[i, j] = ReadInt($"a[{i},{j}] = ");
+
+        if (n % 2 == 0)
+        {
+            int r1 = n / 2 - 1;
+            int r2 = n / 2;
+
+            for (int j = 0; j < n; j++)
+            {
+                int temp = a[r1, j];
+                a[r1, j] = a[r2, j];
+                a[r2, j] = temp;
+            }
+        }
+        else
+        {
+            int mid = n / 2;
+
+            for (int j = 0; j < n; j++)
+            {
+                int temp = a[0, j];
+                a[0, j] = a[mid, j];
+                a[mid, j] = temp;
+            }
+        }
+
+        Console.WriteLine("Результат:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+                Console.Write(a[i, j] + "\t");
+            Console.WriteLine();
+        }
+    }
+
+    static void Task4()
+    {
+        int n = ReadInt("Кількість рядків: ");
+        int[][] arr = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            int m = ReadInt($"Кількість елементів у рядку {i}: ");
+            arr[i] = new int[m];
+
+            for (int j = 0; j < m; j++)
+                arr[i][j] = ReadInt($"arr[{i}][{j}] = ");
+        }
+
+        int maxCols = 0;
+        for (int i = 0; i < n; i++)
+            if (arr[i].Length > maxCols)
+                maxCols = arr[i].Length;
+
+        int[] result = new int[maxCols];
+
+        for (int j = 0; j < maxCols; j++)
+        {
+            result[j] = 0; 
+
+            for (int i = 0; i < n; i++)
+            {
+                if (j < arr[i].Length && arr[i][j] > 0)
+                {
+                    result[j] = arr[i][j];
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine("Результат:");
+        for (int i = 0; i < result.Length; i++)
+            Console.Write(result[i] + " ");
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\n1 - Завдання 1");
+            Console.WriteLine("2 - Завдання 2");
+            Console.WriteLine("3 - Завдання 3");
+            Console.WriteLine("4 - Завдання 4");
+            Console.WriteLine("0 - Вихід");
+
+            int choice = ReadInt("Ваш вибір: ");
+
+            switch (choice)
+            {
+                case 1: Task1(); break;
+                case 2: Task2(); break;
+                case 3: Task3(); break;
+                case 4: Task4(); break;
+                case 0: return;
+                default: Console.WriteLine("Невірний вибір"); break;
+            }
+        }
+    }
+}
